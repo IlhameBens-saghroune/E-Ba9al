a9f144f5efc964a0dff290bf5e90f1206e270d45
ILHAME BENS <ilhambens05@gmail.com>
2022-04-09 23:00:34 +0000

    premier

---
+++ app/src/main/java/com/example/e_ba9al/Lait.java
@@ -0,0 +1,14 @@
+package com.example.e_ba9al;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lait extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lait);
+    }
+}
